690f6b70f2ee3aa659f5a57b0b225f2be3f6147d
lintangms <lintangmuhammadshiddiq@gmail.com>
2022-08-05 20:53:52 +0700

    first commit

---
+++ js/scriptmusic.js
@@ -0,0 +1,9 @@
+let allmusic=[
+    {
+        name: "Bintang Di Surga",
+        artist: "NOAH",
+        img: "BintangDiSurga",
+        src: "NOAH - Bintang di Surga (Official Music Video)"
+    }
+
+]
